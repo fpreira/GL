76c48f5a972fa2442c7573bc275f0f92d7187ca1
fabrice preira <fabrice.preira@ge1.iut-cachan.u-psud.fr>
2018-11-27 09:34:22 +0100

    bibliotheques

---
+++ exo_58~cvi092812.c
@@ -0,0 +1,15 @@
+
+void main(void)
+{
+
+unsigned char combi_joueur[6] =
+{30,47,11,29,16,7};
+unsigned char combi_win[6] =
+{45,47,24,11,17,3};
+unsigned char i=0;
+
+{
+for(i=0;i<6;i=i+1)
+{
+	if combi_joueur[i]
+	
